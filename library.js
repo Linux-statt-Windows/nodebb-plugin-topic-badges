408470609cc48280dd302a83e29285dd4fed248b
Raphael Beer <rbeerdev@gmail.com>
2015-11-12 08:20:20 +0100

    escape badge text in backend

--- library.js
+++ library.js
@@ -6,7 +6,8 @@
 	var topics = module.parent.require('./topics.js'),
 		privileges = module.parent.require('./privileges'),
 		async = module.parent.require('async'),
-		PluginSocket = module.parent.require('./socket.io/plugins');
+		PluginSocket = module.parent.require('./socket.io/plugins'),
+		validator = require('validator');
 
 	TopicBadges.init = function(params, callback) {
 		PluginSocket.TopicBadges = {
@@ -46,24 +47,14 @@
 			callback = text;
 			text = '';
 		}
+		// escape text to prevent XSS
+		text = validator.escape(text);
 		topics.setTopicField(tid, 'badge', text, callback);
 	};
 
 	TopicBadges.addScripts = function(scripts, callback) {
 		scripts.push('plugins/nodebb-plugin-topic-badges/lib/main.js');
 		callback(null, scripts);
-	};
-
-	TopicBadges.addBadgesToTopics = function(data, callback) {
-		async.map(data.topics, function(topic, next) {
-			if (void 0 !== topic.badge && topic.badge !== '') {
-				topic.title = '<span class="badge topic-badge pull-right">' + topic.badge +
-											'</span> ' + topic.title;
-			}
-			return next(null, topic);
-		}, function(err, topics) {
-			return callback(err, data);
-		});
 	};
 
 	TopicBadges.addThreadTools = function(data, callback) {
